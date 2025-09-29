b46e1e797bab9697cc1d3e07ea6934a468455d33
NDC Swift <noah_carpenter@hotmail.com>
2025-09-29 14:12:18 -0400

    Initial Commit

---
+++ ScrollTestUIKit/ViewController.swift
@@ -0,0 +1,60 @@
+//
+    // Project: ScrollTestUIKit
+    //  File: ViewController.swift
+    //  Created by Noah Carpenter
+    //  🐱 Follow me on YouTube! 🎥
+    //  https://www.youtube.com/@NoahDoesCoding97
+    //  Like and Subscribe for coding tutorials and fun! 💻✨
+    //  Fun Fact: Cats have five toes on their front paws, but only four on their back paws! 🐾
+    //  Dream Big, Code Bigger
+    
+
+import UIKit
+
+class ViewController: UIViewController {
+    let scrollView = UIScrollView()
+    let contentView = UIView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupScrollView()
+        addContent()
+    }
+    
+    func setupScrollView() {
+        view.addSubview(scrollView)
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        scrollView.addSubview(contentView)
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            contentView.heightAnchor.constraint(equalToConstant: 1200) // Enables scrolling
+        ])
+    }
+    
+    func addContent() {
+        let label = UILabel()
+        label.text = "Welcome to UIKit ScrollView!"
+        label.textAlignment = .center
+        contentView.addSubview(label)
+        
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+            label.centerXAnchor.constraint(equalTo: contentView.centerXAnchor)
+        ])
+    }
+}
+
+
